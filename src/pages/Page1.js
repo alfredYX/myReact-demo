03b086a449bff4556b8249d2d9f031d158cb0556
alfred <apple259365219@qq.com>
2019-09-30 14:40:47 +0800

    上传图片功能 下载

--- src/pages/Page1.js
+++ src/pages/Page1.js
@@ -1,12 +1,76 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import { Upload, Icon, message } from 'antd'
 import  "./page1.css"
 
+  function getBase64(img, callback) {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => callback(reader.result));
+    reader.readAsDataURL(img);
+  }
+  
+  function beforeUpload(file) {
+    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+    if (!isJpgOrPng) {
+      message.error('You can only upload JPG/PNG file!');
+    }
+    const isLt2M = file.size / 1024 / 1024 < 2;
+    if (!isLt2M) {
+      message.error('Image must smaller than 2MB!');
+    }
+    return isJpgOrPng && isLt2M;
+  }
 class Page1 extends React.Component{
+    state = {
+        loading: false,
+        picture: ''
+    }
+    handleChange = info => {
+        if (info.file.status === 'uploading') {
+          this.setState({ loading: true });
+          return;
+        }
+        if (info.file.status === 'done') {
+          // Get this url from response in real world.
+          getBase64(info.file.originFileObj, imageUrl =>
+            this.setState({
+              imageUrl,
+              loading: false,
+            })
+          );
+        }
+      }
+      getImgData(){ //请求图片数据
+        fetch(`http://127.0.0.1:8081/home/getImg`,{
+        method: 'GET',
+        responseType: 'blob'
+        }).then(res => {
+            console.info(res)
+            if(res.status === 200){
+                this.setState({picture:res.url})  
+            }
+        }).catch(e => console.log('错误:', e))
+        // }).then(res => res.json()).then(data => {
+        //    console.info(data)
+        //    if(data.code === 200){
+        //        let {imgUrl} = data.data
+        //        this.setState({picture:imgUrl})  
+        //    }
+        // }).catch(e => console.log('错误:', e))
+      }
+
+      componentWillMount(){
+        this.getImgData()
+      }
+      
 render(){
-    let page1Style={
-    
-}
+    let page1Style={}
+    const uploadButton = (
+        <div>
+          <Icon type={this.state.loading ? 'loading' : 'plus'} />
+          <div className="ant-upload-text">Upload</div>
+        </div>
+      );
 return(
 <div>
     <div className="page1Style" style={page1Style}>This is Page1!</div>
@@ -14,6 +78,18 @@ return(
             <Link to="/">
                 <div>返回</div>
             </Link>
+      <Upload
+        name="avatar"
+        listType="picture-card"
+        className="avatar-uploader"
+        showUploadList={false}
+        action="http://127.0.0.1:8081/home/uploads"
+        beforeUpload={beforeUpload}
+        onChange={this.handleChange}
+      >
+        {this.state.imageUrl ? <img src={this.state.imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
+      </Upload>
+      <img src={this.state.picture} alt="avatar" style={{ width: '100%' }} />
 </div>
 </div>
 )
